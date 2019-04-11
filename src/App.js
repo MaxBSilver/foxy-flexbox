17f0bd8db6eaae473da49d3e7bf2cfa04e2bbe94
Max Silver <Maxbsilver@gmail.com>
2019-04-11 16:56:25 -0600

    Fetch test API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Main from './components/main/Main'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Main/>
       </div>
     );
   }
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,13 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
 
 export class Main extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+          justifyData : [],
+          alignData : [],
+          isLoading : true
+         
+      }
+    }
+
+    componentDidMount() {
+      this.fetch()
+    }
+    
+    fetch = () => {
+        console.log('fetching')
+        axios("https://gist.githubusercontent.com/MaxBSilver/826e01609a4df3ee1a50c9fed01e355b/raw/983c42081ad596ee3c62520f27627e1a673222ba/foxy-flex-box.json")
+        .then(res => {
+            this.setState({
+                justifyData : res.data.justify,
+                alignData : res.data.align,
+                isLoading : false
+            })
+        })
+    }
+    
+    render() {
+        return !this.state.isLoading 
+        ?<div>
+        </div>
+        :<div>
+            <h1>Loading</h1>
+        </div>
+    }
 }
 
 export default Main
