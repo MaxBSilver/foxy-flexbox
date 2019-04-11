d68bf6d541cda4de082c1b86806837e80b0ebab6
Max Silver <Maxbsilver@gmail.com>
2019-04-11 17:06:26 -0600

    Move fetch from Main to App and restructure Main

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
 import React, { Component } from 'react';
 import './App.css';
+
+import Header from './components/header/Header'
 import Main from './components/main/Main'
+import Footer from './components/footer/Footer'
+
+
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      justifyData : [],
+      alignData : [],
+      isLoading : true
+    }
+  }
+  
+  componentDidMount() {
+    this.fetch()
+  }
+
+  fetch = () => {
+      console.log('fetching')
+      axios("https://gist.githubusercontent.com/MaxBSilver/826e01609a4df3ee1a50c9fed01e355b/raw/983c42081ad596ee3c62520f27627e1a673222ba/foxy-flex-box.json")
+      .then(res => {
+          this.setState({
+              justifyData : res.data.justify,
+              alignData : res.data.align,
+              isLoading : false
+          })
+      })
+  }
+  
   render() {
     return (
       <div className="App">
-        <Main/>
+        <React.Fragment>
+          <Header {...this.state}/>
+          <Main {...this.state}/>
+          <Footer />
+        </React.Fragment>
       </div>
     );
   }
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,42 +1,11 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from 'react'
+import Answer from '../answer/Answer'
 
-export class Main extends Component {
-    constructor(props) {
-      super(props)
-    
-      this.state = {
-          justifyData : [],
-          alignData : [],
-          isLoading : true
-         
-      }
-    }
-
-    componentDidMount() {
-      this.fetch()
-    }
-    
-    fetch = () => {
-        console.log('fetching')
-        axios("https://gist.githubusercontent.com/MaxBSilver/826e01609a4df3ee1a50c9fed01e355b/raw/983c42081ad596ee3c62520f27627e1a673222ba/foxy-flex-box.json")
-        .then(res => {
-            this.setState({
-                justifyData : res.data.justify,
-                alignData : res.data.align,
-                isLoading : false
-            })
-        })
-    }
-    
-    render() {
-        return !this.state.isLoading 
-        ?<div>
-        </div>
-        :<div>
-            <h1>Loading</h1>
-        </div>
-    }
+export default function Main() {
+  return (
+    <main>
+        <Answer/>
+      
+    </main>
+  )
 }
-
-export default Main
