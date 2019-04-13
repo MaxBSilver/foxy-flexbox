54a456bd73ab91dad9dc6a29602c16ec2b32e361
Max Silver <Maxbsilver@gmail.com>
2019-04-13 14:55:29 -0600

    headaer styling and board styling

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Header(props) {
   return (
     <header>
-      <h1 className='header-title'>fox flexbox</h1>
+      <h1 className='header-title'><img alt='logo' src='http://www.logospng.com/images/101/fox-logo-animal-on-behance-pinterest-101998.jpg'/><span className="header-white-text">fox </span><span className='header-colored-letters'>flex</span>box</h1>
     </header>
   );
 }
