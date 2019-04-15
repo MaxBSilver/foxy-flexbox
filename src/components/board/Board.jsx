e6b8b403ea3ca76c0d8413ad6d17109ce11e3f46
Max Silver <Maxbsilver@gmail.com>
2019-04-15 12:42:39 -0600

    created a hard board

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 import EasyBoard from "./easy-board/EasyBoard";
 import ModerateBoard from "./moderate-board/ModerateBoard";
+import HardBoard from './hard-board/HardBoard';
 import { useSpring, animated } from "react-spring";
 
 export default function Board(props) {
   const springProps = useSpring({ opacity: 1, from: { opacity: 0 } });
-  return props.roundNumber <= 5 ? (
-    <animated.div style={springProps}>
-      <EasyBoard {...props} />
-    </animated.div>
-  ) : (
-    <animated.div style={springProps}>
-      <ModerateBoard {...props} />
-    </animated.div>
-  );
+  if (props.roundNumber <= 5) {
+    return (
+      <animated.div style={springProps}>
+        <EasyBoard {...props} />
+      </animated.div>
+    );
+  } else if (props.roundNumber <= 20) {
+    return (
+      <animated.div style={springProps}>
+        <ModerateBoard {...props} />
+      </animated.div>
+    );
+  } else if (props.roundNumber > 20) {
+    return (
+      <animated.div style={springProps}>
+        <HardBoard {...props} />
+      </animated.div>
+    );
+  }
 }
