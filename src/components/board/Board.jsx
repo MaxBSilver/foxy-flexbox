61fee1ab7e23485894c422b50b9087ac46aac012
Max Silver <Maxbsilver@gmail.com>
2019-04-15 12:28:24 -0600

    styling button to change on correct answer and button cannot go past round range'

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import EasyBoard from "./easy-board/EasyBoard";
 import ModerateBoard from "./moderate-board/ModerateBoard";
+import { useSpring, animated } from "react-spring";
+
 export default function Board(props) {
+  const springProps = useSpring({ opacity: 1, from: { opacity: 0 } });
   return props.roundNumber <= 5 ? (
-    <EasyBoard {...props} />
+    <animated.div style={springProps}>
+      <EasyBoard {...props} />
+    </animated.div>
   ) : (
-    <ModerateBoard {...props} />
+    <animated.div style={springProps}>
+      <ModerateBoard {...props} />
+    </animated.div>
   );
 }
--- src/components/prompt/Prompt.jsx
+++ src/components/prompt/Prompt.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretLeft, faCaretRight } from "@fortawesome/free-solid-svg-icons";
+import { useSpring, animated } from "react-spring";
+
 export default function Prompt(props) {
   const nextRound = () => {
     props.incrementRound();
@@ -9,36 +11,40 @@ export default function Prompt(props) {
   const previousRound = () => {
     props.decrementRound();
   };
-  const { difficulty, prompt, id } = props;
+  const { difficulty, id } = props;
+  const springProps = useSpring({ opacity: 1, from: { opacity: 0 } });
   return (
-    <section className="prompt--container">
-      <div className="prompt-head--container">
-        <h3 className="prompt-difficulty">Difficulty : {difficulty}</h3>
-        <div className="prompt-button--container">
-          <button className="prompt-previous-button" onClick={previousRound}>
-            <FontAwesomeIcon icon={faCaretLeft} />
-          </button>
-          <h3> Level : {id} of 20</h3>
-          <button className="prompt-next-button" onClick={nextRound}>
-            <FontAwesomeIcon icon={faCaretRight} />
-          </button>
+    <animated.div style={springProps}>
+      <section className="prompt--container">
+        <div className="prompt-head--container">
+          <h3 className="prompt-difficulty">Difficulty : {difficulty}</h3>
+          <div className="prompt-button--container">
+            <button className="prompt-previous-button" onClick={previousRound}>
+              <FontAwesomeIcon icon={faCaretLeft} />
+            </button>
+            <h3> Level : {id} of 20</h3>
+            <button className="prompt-next-button" onClick={nextRound}>
+              <FontAwesomeIcon icon={faCaretRight} />
+            </button>
+          </div>
         </div>
-      </div>
-      <div className="prompt-body-container">
-        <p className="prompt-body-title">
-          Help the fox get into the box by using the following <span>CSS properties : </span>
-        </p>
-        <div className="prompt-property--container">
-          <ul>
-            <li> center</li>
-            <li> flex-start</li>
-            <li> flex-end</li>
-            <li> space-around</li>
-            <li> space-between</li>
-            <li> space-evenly</li>
-          </ul>
+        <div className="prompt-body-container">
+          <p className="prompt-body-title">
+            Help the fox get into the box by using the following{" "}
+            <span>CSS properties : </span>
+          </p>
+          <div className="prompt-property--container">
+            <ul>
+              <li> center</li>
+              <li> flex-start</li>
+              <li> flex-end</li>
+              <li> space-around</li>
+              <li> space-between</li>
+              <li> space-evenly</li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </animated.div>
   );
 }
