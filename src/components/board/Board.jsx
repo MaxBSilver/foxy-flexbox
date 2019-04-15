6d472450e8e6d827d9b8bdffe169685353b6706d
Max Silver <Maxbsilver@gmail.com>
2019-04-15 14:10:10 -0600

    Revert "created a hard board"

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -1,28 +1,17 @@
 import React from "react";
 import EasyBoard from "./easy-board/EasyBoard";
 import ModerateBoard from "./moderate-board/ModerateBoard";
-import HardBoard from './hard-board/HardBoard';
 import { useSpring, animated } from "react-spring";
 
 export default function Board(props) {
   const springProps = useSpring({ opacity: 1, from: { opacity: 0 } });
-  if (props.roundNumber <= 5) {
-    return (
-      <animated.div style={springProps}>
-        <EasyBoard {...props} />
-      </animated.div>
-    );
-  } else if (props.roundNumber <= 20) {
-    return (
-      <animated.div style={springProps}>
-        <ModerateBoard {...props} />
-      </animated.div>
-    );
-  } else if (props.roundNumber > 20) {
-    return (
-      <animated.div style={springProps}>
-        <HardBoard {...props} />
-      </animated.div>
-    );
-  }
+  return props.roundNumber <= 5 ? (
+    <animated.div style={springProps}>
+      <EasyBoard {...props} />
+    </animated.div>
+  ) : (
+    <animated.div style={springProps}>
+      <ModerateBoard {...props} />
+    </animated.div>
+  );
 }
