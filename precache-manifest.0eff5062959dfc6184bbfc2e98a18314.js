c52c2593dbde1730718c0dc2777631f9206d3911
Max Silver <Maxbsilver@gmail.com>
2019-04-17 12:41:49 -0600

    Updates

---
+++ precache-manifest.0eff5062959dfc6184bbfc2e98a18314.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "51d03a1b0fe2dd531c12",
+    "url": "/foxy-flexbox/static/css/main.3933a8b2.chunk.css"
+  },
+  {
+    "revision": "51d03a1b0fe2dd531c12",
+    "url": "/foxy-flexbox/static/js/main.d57d6baf.chunk.js"
+  },
+  {
+    "revision": "6de19b57e6a90f2b005c",
+    "url": "/foxy-flexbox/static/js/runtime~main.b7e4e9c5.js"
+  },
+  {
+    "revision": "a0b03289de4ebcf88773",
+    "url": "/foxy-flexbox/static/js/2.ea647a0a.chunk.js"
+  },
+  {
+    "revision": "aa60f046207fb7d749a8070e8e7e1871",
+    "url": "/foxy-flexbox/index.html"
+  }
+];
